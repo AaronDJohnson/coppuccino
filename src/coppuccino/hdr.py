2e2bafd3201215d823225ce3f13d350095e6fc86
Aaron <Aaron9034@Hotmail.com>
2025-10-03 03:35:05 -0700

    set NF default kwargs

--- src/coppuccino/hdr.py
+++ src/coppuccino/hdr.py
@@ -4,7 +4,18 @@ from coppuccino.copula_flows import sample_and_log_prob, log_prob
 
 
 def compute_injection_hdr(samples: np.ndarray, injection_params: np.ndarray, num_samples: int = 100_000, return_flow=False, **nf_kwargs):
-    default_kwargs = {'knots':64, 'interval':16, 'patience':20, 'learning_rate':1e-4, 'max_epochs':400, 'nn_depth':16}
+    if injection_params.ndim == 0:
+        raise ValueError("injection_params must be at least 1D")
+    default_kwargs = {'knots':4, 'patience':30, 'learning_rate':1e-3, 'max_epochs':400, 'flow_layers':6}
+    # default_kwargs = {'knots': 32,
+    #                   'patience': 20,
+    #                   'learning_rate': 1e-4,
+    #                   'max_epochs': 200,
+    #                   'maf_layers': 8,
+    #                   'spline_layers': 8,
+    #                   'nn_depth': 2,
+    #                   'nn_width': 128,
+    #                   'use_maf': True}
     kwargs = nf_kwargs if nf_kwargs else default_kwargs
     # fit NF to samples
     flow = normalizing_flows_fit(samples, **kwargs)  # TODO: document kwargs in docstring
@@ -13,9 +24,8 @@ def compute_injection_hdr(samples: np.ndarray, injection_params: np.ndarray, num
     injection_probs = log_prob(flow, injection_params)
 
     hdrs = []
-    if injection_params.ndim == 0:
-        raise ValueError("injection_params must be at least 1D")
-    elif injection_params.ndim == 1:
+
+    if injection_params.ndim == 1:
         fraction = np.sum(gen_log_probs >= injection_probs) / num_samples
         hdrs.append(fraction)
     else:
